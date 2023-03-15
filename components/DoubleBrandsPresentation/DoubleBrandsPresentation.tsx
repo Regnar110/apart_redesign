8c2ba4e547bbe0755db448a65c6c4767eca00f26
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-03-16 00:26:23 +0100

    Added DoubleBrandPresentation structure for futher implementations

--- components/DoubleBrandsPresentation/DoubleBrandsPresentation.tsx
+++ components/DoubleBrandsPresentation/DoubleBrandsPresentation.tsx
@@ -3,31 +3,46 @@ import Image from 'next/image'
 import CustomTextButton from '../button/CustomTextButton'
 import charms_desktop from '../../public/DoubleBrands/set_one/charms_desktop.jpg'
 import charms_mobile from '../../public/DoubleBrands/set_one/charms_mobile.webp'
+import MediaQuery from 'react-responsive'
 
 
 const DoubleBrandsPresentation = () => {
   return (
-    <div className='double_brands_container flex flex-col lg:grid lg:grid-cols-2 gap-10'>
-       <div className='brand_container flex flex-col justify-center items-center relative text-center '>
-            <div className='brand_image relative w-[100vw]] h-[100%] m-auto'>
-                <Image fill={true} style={{objectFit:"contain"}} src={charms_desktop} alt="brand"/>
-            </div>
-            <h3>Pióra Wieczne</h3>
-            <p>Prezent z klasą dla każdego, kto ceni nienaganny styl i prestiż. Wybierz pióro wieczne renomowanych marek, od lat cieszących się uznaniem na rynku</p>
-            <div className='brand_button_width_setter w-[70px]'>
-                <CustomTextButton isArrow={true} textContent="" hrefQuery='/'/>
-            </div>
-       </div>
-       <div className='brand_container flex flex-col justify-center items-center w-full h-[40vw] relative text-center '>
-            <div className='brand_image relative w-[100%] h-[100%] m-auto'>
-                <Image fill={true} style={{objectFit:"contain"}} src={charms_desktop} alt="brand"/>
-            </div>
-            <h3>Pióra Wieczne</h3>
-            <p>Prezent z klasą dla każdego, kto ceni nienaganny styl i prestiż. Wybierz pióro wieczne renomowanych marek, od lat cieszących się uznaniem na rynku</p>
-            <div className='brand_button_width_setter w-[70px]'>
-                <CustomTextButton isArrow={true} textContent="" hrefQuery='/'/>
-            </div>
-       </div>
+    <div className='double_brands_container md:flex md:justify-center md:items-center  gap-10 lg:w-[100%] xl:w-[70%] p-0 md:p-5'>
+      <div className='brand_card flex flex-col w-[100%] md:w-[50%] justify-center items-center'>
+        <div className='card_image_container relative flex justify-center items-center h-[100%]'>
+          <MediaQuery maxWidth={767}>
+            <Image src={charms_mobile} style={{objectFit:"contain"}} alt="card_brand"/>
+          </MediaQuery>
+          <MediaQuery minWidth={768}>
+            <Image src={charms_desktop} style={{objectFit:"contain"}} alt="card_brand"/>
+          </MediaQuery>
+        </div>
+        <div className='brand_card_description flex flex-col text-center'>
+          <h3 className='font-playfair text-[25px] sm:text-[30px] md:text-[35px] lg:text-[35px] xl:text-[40px] 2xl:text-[50px]'>Pióra wieczne</h3>
+          <p className='font-roboto text-[13px] sm:text-[14px] md:text-[14px] lg:text-[15px] xl:text-[17px] 2xl:text-[18px]'>Prezent z klasą dla każdego, kto ceni nienaganny styl i prestiż. Wybierz pióro wieczne renomowanych marek, od lat cieszących się uznaniem na rynku</p>
+        </div>
+        <div className='w-[70px]'>
+          <CustomTextButton textContent='' isArrow={true} hrefQuery={'/'}/>
+        </div>
+      </div>
+      <div className='brand_card w-[100%] md:w-[50%] flex flex-col justify-center items-center'>
+        <div className='card_image_container flex justify-center items-center relative h-[100%]'>
+          <MediaQuery maxWidth={767}>
+            <Image src={charms_mobile} style={{objectFit:"contain"}} alt="card_brand"/>
+          </MediaQuery>
+          <MediaQuery minWidth={768}>
+            <Image src={charms_desktop} style={{objectFit:"contain"}} alt="card_brand"/>
+          </MediaQuery>
+        </div>
+        <div className='brand_card_description flex flex-col text-center'>
+          <h3 className='font-playfair text-[25px] sm:text-[30px] md:text-[35px] lg:text-[35px] xl:text-[40px] 2xl:text-[50px]'>Pióra wieczne</h3>
+          <p className='font-roboto text-[13px] sm:text-[14px] md:text-[14px] lg:text-[15px] xl:text-[17px] 2xl:text-[18px]'>Prezent z klasą dla każdego, kto ceni nienaganny styl i prestiż. Wybierz pióro wieczne renomowanych marek, od lat cieszących się uznaniem na rynku</p>
+        </div>
+        <div className='w-[70px]'>
+          <CustomTextButton textContent='' isArrow={true} hrefQuery={'/'}/>
+        </div>
+      </div>
     </div>
   )
 }
