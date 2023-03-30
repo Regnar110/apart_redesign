a192a5a28e88463444563856130135edf5322a6b
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-03-31 00:36:25 +0200

    finished product page for now. Now going to implement logic for basket and wishlist

--- pages/product/[...product_id].tsx
+++ pages/product/[...product_id].tsx
@@ -13,17 +13,21 @@ import MediaQuery from 'react-responsive';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Scrollbar } from "swiper";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 import ModeOfTravelIcon from '@mui/icons-material/ModeOfTravel';
+import eko_opakowania from '../../public/eko.jpg'
 
 const Product = () => {
 
     // 768PX Mobile break point
     const router = useRouter();
+    
     const {product_id} = router.query // tablica [id produktu, referencja do kategori tego produktu]
     let singleProduct;
     if(product_id && typeof product_id[1] === "string") singleProduct = useSelector((state:RootState) => selectedProduct(state, product_id[1], product_id[0])) // product_id[0] to odniesienie do kategori, w której znajduje się produkt tj product_id[1].
-    console.log(singleProduct)
+    const details = singleProduct ? Object.entries(singleProduct.details) : null
+    console.log(details)
     return(
         <div className='product_page box-border overflow-x-hidden'>
             <Head>
@@ -102,15 +106,85 @@ const Product = () => {
                         </div>
                     </div>
                 </div>
-                <div className='product_details_section'>
-                    details
+                <div className='product_details_section flex flex-col lg:flex-row justify-center gap-x-5'>
+                    <div className='details w-full md:w-[90%] lg:w-[500px] xl:w-[600px] flex flex-col gap-y-5 pt-10'>
+                        <div className='box_title text-[17px] border-b-[1px] border-black'>
+                            <h4>Cechy produktu</h4>
+                        </div>
+                        <div className='product_properties flex flex-col'>
+                            {
+                                details!.map(el => {
+                                    return (
+                                        <div className='property text-[14px] flex gap-x-4'>
+                                            <span className='font-semibold'>{el[0]}:</span>
+                                            <span className='font-light'>{el[1]}</span>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                    <div className='description w-full md:w-[90%] lg:w-[500px] xl:w-[600px] flex flex-col gap-y-5 justify-start pt-10'>
+                        <div className='box_title text-[17px] border-b-[1px] border-black'>
+                            <h4>Opis produktu</h4>
+                        </div>
+                        <span className='text-[14px] font-light'>
+                            {singleProduct?.Opis[0].children[0].text}
+                        </span>
+                    </div>
+                </div>
+                <div className='eko_opakowania relative flex flex-col lg:flex-row justify-center text-left gap-x-5 pt-10'>
+                    <div className='opis_eko flex flex-col gap-y-5 w-full lg:w-[500px] xl:w-[600px]'>
+                        <div className='box_title text-[17px] border-b-[1px] border-black'>
+                            <h4>Piękne opakowania gratis</h4>
+                        </div>
+                        <span className='text-[14px]'>Kupione na apart.pl produkty są domyślnie pakowane w ekologiczne opakowania, które wprowadziliśmy w trosce o ochronę środowiska. Są one estetyczne, a co najważniejsze przyjazne naszej planecie. W eSklepie Apart są dostępne także nowe opakowania w zachwycającej subtelnością tonacji pudrowego różu. Darmowy zestaw to wybrane piękne pudełko oraz dołączona torebka upominkowa.</span>
+                    </div>
+                    <Image className='w-full h-[350px] lg:h-auto lg:w-[500px] xl:w-[600px]' width={415} height={335} style={{objectFit:"contain"}} src={eko_opakowania} alt={'eko_opakowania'} />   
                 </div>                 
             </div>
             <RandomAmountProducts header={'Produkty do kompletu'} />
             <RandomAmountProducts header={'Najczęściej wybierane'} />
-            <div className='products_help flex'>
-                <div className='customer_help'>customer help</div>
-                <div className='ask_for_product'>ask for product</div>
+            <div className='products_help flex flex-col lg:flex-row justify-center gap-5 px-5 lg:p-0'>
+                <div className='customer_help w-full lg:w-[500px] xl:w-[600px] flex flex-col justify-start pt-10'>
+                    <div className='box_title text-[17px] border-b-[1px] border-black mb-5'>
+                        <h4>Pomoc przy zakupie</h4>
+                    </div>
+                    <div className='text-[14px] text-black transition-all hover:text-yellow-500 cursor-pointer font-light'><ChevronRightIcon fontSize='small'/><span>Ekspert w biżuterii</span></div>
+                    <div className='text-[14px] pt-1 text-black transition-all hover:text-yellow-500 cursor-pointer font-light'><ChevronRightIcon fontSize='small'/><span>Jak wybrać właściwy rozmiar pierścionka</span></div>
+                    <div className='text-[14px] pt-1 text-black transition-all hover:text-yellow-500 cursor-pointer font-light'><ChevronRightIcon fontSize='small'/><span>Jak kupować w eSklepie</span></div>
+                    <div className='text-[14px] pt-1 text-black transition-all hover:text-yellow-500 cursor-pointer font-light'><ChevronRightIcon fontSize='small'/><span>Wysyłka i formy płatności</span></div>
+                    <div className='text-[14px] pt-1 text-black transition-all hover:text-yellow-500 cursor-pointer font-light'><ChevronRightIcon fontSize='small'/><span>Apart Diamond Club</span></div>
+                    <div className='text-[14px] pt-1 text-black transition-all hover:text-yellow-500 cursor-pointer font-light'><ChevronRightIcon fontSize='small'/><span>Regulamin sprzedaży</span></div>
+                </div>
+                <div className='ask_for_product w-full lg:w-[500px] xl:w-[600px] flex flex-col gap-y-5 justify-start pt-10'>
+                    <div className='box_title text-[17px] border-b-[1px] border-black'>
+                        <h4>Zapytaj o produkt</h4>
+                    </div>
+                    <div className='ask flex flex-col'>
+                    <div className='checkbox_ask flex gap-3'>
+                       <input className='cursor-pointer' defaultChecked={true} type="checkbox" placeholder="Zapytaj o dostęp" /> 
+                       <label className='cursor-pointer'>Zapytaj o dostępność produktu</label>
+                    </div>
+                    <div className='checkbox_ask flex gap-3'>
+                       <input className='cursor-pointer' type="checkbox" placeholder="Zapytaj o dostęp" /> 
+                       <label className='cursor-pointer'>Zapytaj o parametry produktu</label>
+                    </div>
+                     <form className='ask_question form pt-3'>
+                        <label className='font-semibold'>Zadaj pytanie</label>
+                        <textarea className=' border-[1px] border-black w-full h-[200px]' required={true} minLength={20}/>
+                        <span className='text-[11px] font-light'>(wymagane, min. 20 znaków)</span>
+                        
+                        <div className='ask_question_email flex flex-col'>
+                            <label className='font-semibold pt-3'>Adres email</label>
+                            <div className='email_as_input flex items-end flex-col md:flex-row gap-2'>
+                                <input className='border-[1px] w-full  border-black' type='email' required={true}/>
+                                <button type='submit' className='p-[2px] shadow-lg w-[150px] bg-[#F4C1C5] text-[#ae535a] hover:bg-[#c7747b] hover:text-white' >Wyślij</button>
+                            </div>
+                        </div>
+                     </form>
+                    </div>
+                </div>
             </div>
             <Footer/>
         </div>
